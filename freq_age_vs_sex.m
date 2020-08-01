1137f1ff154ca6e3e7691104bf7c2f2f11701b5e
Sufien Tout <sufientout@gmail.com>
2020-08-01 15:11:55 -0400

    did a little touch up

--- freq_age_vs_sex.m
+++ freq_age_vs_sex.m
@@ -13,28 +13,46 @@ nan_sex = sum(ismissing(sex));
 invalid_age = length(find(age < 0));
 invalid_sex = length(find(sex < 0 | sex > 1));
 
-% outliers
-age_outs = find(isoutlier(age', 'quartile'));
+% defining the constants
+MALE = 0;
+FEMALE = 1;
 
-% split data into 2 series
-male = age(sex == 0);
-female = age(sex == 1);
+% splitting data
+male_ages = age(sex == MALE);
+female_ages = age(sex == FEMALE);
+
+% outliers
+male_age_outs = find(isoutlier(male_ages', 'quartile'));
+female_age_outs = find(isoutlier(female_ages', 'quartile'));
+
+% summary stats
+disp('Male Stats');
+min(male_ages)
+max(male_ages)
+mean(male_ages)
+median(male_ages)
+
+disp('Female Stats');
+min(female_ages)
+max(female_ages)
+mean(female_ages)
+median(female_ages)
+
+% plot box-plot
+subplot(2, 1, 1);
+group = [ones(size(male_ages)); 2 * ones(size(female_ages))];
+boxplot([male_ages; female_ages], group, 'Labels', {'Male', 'Female'});
+title('Ages of Heart Attack Patients')
+xlabel('Sex')
+ylabel('Age')
 
 % plot freq-histogram
-subplot(2, 1, 1);
-histogram(male);
+subplot(2, 1, 2);
+histogram(male_ages);
 hold on
-histogram(female);
+histogram(female_ages);
 hold off
 title('Heart Attack Age-Freq vs. Sex');
 xlabel('Age Groups');
 ylabel('Frequency');
 legend('Male','Female');
-
-% plot box-plot
-subplot(2, 1, 2);
-group = [ones(size(male)); 2 * ones(size(female))];
-boxplot([male; female], group, 'Labels', {'Male', 'Female'});
-title('Ages of Heart Attack Patients')
-xlabel('Sex')
-ylabel('Age')
